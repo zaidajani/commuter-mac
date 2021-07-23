d396e509e65e7d5f1a405bcf1bab6169f3bf6559
zaidajani <zaid.metal.ajani@gmail.com>
2021-07-23 23:19:44 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    // titleBarStyle: "hidden",
+    width: 800,
+    height: 600,
+    minWidth: 1281,
+    minHeight: 800,
+    backgroundColor: "#FFFFFF",
+  });
+  // mainWindow.setMenuBarVisibility(false);
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  //
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
